659176dc51c513d7833f37a80d5190d954063da7
Nan Ei Ei Htun <nanneieihtun@gmail.com>
2024-01-26 11:12:34 +0800

    Update dbService.js

--- server/dbService.js
+++ server/dbService.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const dbConnection = mysql.createConnection({
-  host: process.env.VPI_DB_HOST,
-  user: process.env.VPI_DB_USERNAME,
-  password: process.env.VPI_DB_PASSWORD,
-  database: process.env.VPI_DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 dbConnection.connect((err) => {
